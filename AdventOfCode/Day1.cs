e6ed51bc23bf6bdea2ed4a1a694395d2c8297aae
Fiorenzo Rutschmann <fio.rutschmann@gmail.com>
2024-12-01 18:38:55 +1300

    Day 1 done.

--- AdventOfCode/Day1.cs
+++ AdventOfCode/Day1.cs
@@ -2,10 +2,46 @@
 {
     public class Day1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("./test/day1.txt", 11)]
+        [InlineData("./input/day1.txt", 2344935)]
+        public void Part1(string input, int expected)
         {
+            string[] lines = File.ReadAllLines(input);
+            int result = 0;
 
+            //Get the two lists
+            var left = lines.Select(a => int.Parse(a.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).First())).Order();
+            var right = lines.Select(a => int.Parse(a.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Last())).Order();
+
+            var zip = left.Zip(right);
+            
+            foreach (var line in zip)
+            {
+                result += Math.Abs(line.Second - line.First);
+            }
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("./test/day1.txt", 31)]
+        [InlineData("./input/day1.txt", 2344935)]
+        public void Part2(string input, int expected)
+        {
+            string[] lines = File.ReadAllLines(input);
+            int result = 0;
+
+            //Get the two lists
+            var left = lines.Select(a => int.Parse(a.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).First())).Order();
+            var right = lines.Select(a => int.Parse(a.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Last())).Order();
+
+            foreach(var i in left)
+            {
+                result += (i * right.Count(a => a == i));
+            }
+
+            Assert.Equal(expected, result);
         }
     }
 }
