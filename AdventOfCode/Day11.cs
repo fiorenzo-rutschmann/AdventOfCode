0093b0030d03fda6dbdf7cea8928c621e5b555bb
Fiorenzo Rutschmann <fio.rutschmann@gmail.com>
2024-12-11 19:18:30 +1300

    Day 11 added faster part 2, based on part 1.

--- AdventOfCode/Day11.cs
+++ AdventOfCode/Day11.cs
@@ -143,5 +143,82 @@ namespace AdventOfCode
 
             Assert.Equal(expected, result);
         }
+
+
+        [Theory]
+        [InlineData("./test/day11.txt", 65601038650482)]
+        [InlineData("./input/day11.txt", 276661131175807)]
+        public void Part2_faster(string file, long expected)
+        {
+            string input = File.ReadAllText(file);
+            long result = 0;
+
+            List<long> stones1 = input.Split(' ').Select(a => long.Parse(a)).ToList();
+            int blinks = 75;
+
+            Dictionary<long, long> stones = new Dictionary<long, long>();
+            foreach (var stone in stones1)
+            {
+                if (stones.ContainsKey(stone))
+                    stones[stone]++;
+                else
+                    stones.Add(stone, 1);
+            }
+
+            for (int i = 0; i < blinks; i++)
+            {
+                //List<long> newStones = new List<long>();
+                Dictionary<long, long> newStones = new Dictionary<long, long>();
+
+                foreach (long stone in stones.Keys)
+                {
+                    if (stone == 0)
+                    {
+                        if (newStones.ContainsKey(1))
+                            newStones[1] += stones[stone];
+                        else
+                            newStones.Add(1, stones[stone]);
+
+                    }
+                    else if (Convert.ToInt64(Math.Floor(Math.Log10(stone) + 1)) % 2 == 0)//(stone.ToString().Length % 2 == 0)
+                    {
+                        string str = stone.ToString();
+
+                        string left = str.Substring(0, str.Length / 2);
+                        string right = str.Substring(str.Length / 2);
+
+                        long lleft = long.Parse(left);
+                        long lright = long.Parse(right);
+
+                        if (newStones.ContainsKey(lleft))
+                            newStones[lleft] += stones[stone];
+                        else
+                            newStones.Add(lleft, stones[stone]);
+
+                        if (newStones.ContainsKey(lright))
+                            newStones[lright] += stones[stone];
+                        else
+                            newStones.Add(lright, stones[stone]);
+                    }
+                    else
+                    {
+                        long times2024 = stone * 2024;
+
+                        if (newStones.ContainsKey(times2024))
+                            newStones[times2024] += stones[stone];
+                        else
+                            newStones.Add(times2024, stones[stone]);
+                    }
+                }
+
+                stones = newStones;
+            }
+
+            //result += stones.Count;
+            result += stones.Sum(a => a.Value);
+
+
+            Assert.Equal(expected, result);
+        }
     }
 }
