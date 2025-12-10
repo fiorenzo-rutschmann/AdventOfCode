0e6f9e8e181ba18b0c760ebae13540d739f23c20
fiorenzo-rutschmann <fio.rutschmann@gmail.com>
2025-12-10 23:29:39 +1300

    Day10 Part 1

---
+++ AdventOfCode2025/Day10.cs
@@ -0,0 +1,116 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AdventOfCode2025;
+
+public class Day10
+{
+    [Theory]
+    [InlineData("./test/day10.txt", 7)]
+    [InlineData("./input/day10.txt", 459)]
+    public void Part1(string input, long expected)
+    {
+        long result = 0;
+
+        string[] lines = File.ReadAllLines(input);
+
+        Regex regx = new Regex("\\[(?<goal>[\\.#]*)\\]( \\((?<buttons>[0-9,]*)\\))*");
+
+
+        foreach (var line in lines)
+        {
+            var matches = regx.Match(line);
+
+            var buttons = matches.Groups["buttons"].Captures.Select(c => c.Value);
+
+            List<ulong> longButtons = new();
+            foreach (var b in buttons)
+            {
+                ulong bulong = 0x0;
+
+                foreach (var position in b.Split(',').Select(int.Parse))
+                {
+                    bulong |= BitOperations.RotateLeft(1, position);
+                }
+
+                longButtons.Add(bulong);
+            }
+
+            var goal = matches.Groups["goal"].Value;
+            ulong longGoal = 0x0;
+            foreach (var i in goal.Index())
+            {
+                if (i.Item == '#')
+                    longGoal |= BitOperations.RotateLeft(1, i.Index);
+            }
+
+            if (longButtons.Contains(longGoal))
+            {
+                result += 1;
+                continue;
+            }
+
+            //2
+            foreach(var num1 in longButtons.Index())
+            {
+                foreach(var num2 in longButtons.Skip(num1.Index))
+                {
+                    if ((num1.Item ^ num2) == longGoal)
+                    {
+                        result += 2;
+                        goto next;
+                    }
+                }
+            }
+
+            bool IsBitSet(int number, int bitPosition)
+            { 
+                int mask = 1 << bitPosition;
+                return (number & mask) != 0;
+            }
+
+            long lowest = long.MaxValue;
+            for (int i = 1; i < 1 << longButtons.Count(); i++)
+            {
+                ulong test = 0;
+                int pushes = 0;
+                for(int b = 0; b < longButtons.Count(); b++)
+                {
+                    if (IsBitSet(i, b))
+                    {
+                        pushes++;
+                        test ^= longButtons[b];
+                    }
+                }
+
+                if (test == longGoal && pushes < lowest)
+                {
+                    lowest = pushes;
+                }
+            }
+
+            result += lowest;
+
+            next:
+                continue;
+
+        }
+        
+        Assert.Equal(expected, result);
+    }
+
+
+    [Theory]
+    [InlineData("./test/day10.txt", 0)]
+    [InlineData("./input/day10.txt", 0)]
+    public void Part2(string input, long expected)
+    {
+        long result = 0;
+
+        string[] lines = File.ReadAllLines(input);
+
+        Assert.Equal(expected, result);
+    }
+}
--- AdventOfCode2025/Day9.cs
+++ AdventOfCode2025/Day9.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode2025;
+﻿using System.Security.Cryptography;
+
+namespace AdventOfCode2025;
 
 public class Day9
 {
@@ -38,7 +40,7 @@ public class Day9
 
     [Theory]
     [InlineData("./test/day9.txt", 24)]
-    [InlineData("./input/day9.txt", 0)]
+    [InlineData("./input/day9.txt", 1544362560)]
     public void Part2(string input, long expected)
     {
         long result = 0;
@@ -206,6 +208,189 @@ public class Day9
         }
 
         result = maxSize;
+        Assert.Equal(expected, result);
+    }
+
+
+    [Theory]
+    [InlineData("./test/day9.txt", 24)]
+    [InlineData("./input/day9.txt", 1544362560)]
+    public void Part2_optimise1(string input, long expected)
+    {
+        long result = 0;
+
+        string[] lines = File.ReadAllLines(input);
+
+        var coords = lines.Select(line => (long.Parse(line.Split(',').ToArray()[0]), long.Parse(line.Split(',').ToArray()[1]))).ToList();
+
+        bool BetweenInclusive(long i, long x1, long x2)
+        {
+            if (x1 > x2)
+            {
+                return (x1 >= i && i >= x2);
+            }
+
+            if (x1 < x2)
+            {
+                return (x2 >= i && i >= x1);
+            }
+
+            if (x1 == x2 && x1 == i)
+            {
+                return true; //aka on point
+            }
+
+            return false; //no idea
+        }
+
+        int mod(int x, int m)
+        {
+            return (x % m + m) % m;
+        }
+
+        bool CoordIsInPolygon((long x, long y) coord)
+        {
+            int count = 0;
+
+            foreach ((int index, (long x, long y) coord) i in coords.Index())
+            {
+                int nextIndex = (i.index + 1) % coords.Count();
+
+                long thisX = i.coord.x;
+                long thisY = i.coord.y;
+
+                long nextX = coords[nextIndex].Item1;
+                long nextY = coords[nextIndex].Item2;
+
+                //on a line
+                if (thisY == nextY && coord.y == thisY && BetweenInclusive(coord.x, thisX, nextX))
+                {
+                    return true;
+                }
+
+                if (thisX == nextX && coord.x == thisX && BetweenInclusive(coord.y, thisY, nextY))
+                {
+                    return true;
+                }
+
+                if (thisY == nextY && thisY < coord.y && BetweenInclusive(coord.x, thisX, nextX))
+                {
+                    count++;
+                }
+
+                //edgecase runalong.
+                if (thisX == nextX && thisX == coord.x && thisY < coord.y && nextY < coord.y)
+                {
+                    int previousIndex = mod((i.index - 1), coords.Count());
+                    long prevX = coords[previousIndex].Item1;
+                    long prevY = coords[previousIndex].Item2;
+
+                    int nextNextIndex = (i.index + 2) % coords.Count();
+                    long nextNextX = coords[nextNextIndex].Item1;
+                    long nextNextY = coords[nextNextIndex].Item2;
+
+                    if (prevX >= thisX && nextNextX <= thisX)
+                        count++;
+
+                    if (prevX <= thisX && nextNextX >= thisX)
+                        count++;
+                }
+            }
+
+            return (count % 2) == 1;
+        }
+
+        long Size((long x, long y) p1, (long x, long y) p2)
+        {
+            return (Math.Abs(p1.x - p2.x) + 1) * (Math.Abs(p1.y - p2.y) + 1);
+        }
+
+        bool AllCornersInPolygon((long x, long y) p1, (long x, long y) p2)
+        {
+            if (!CoordIsInPolygon((p2.x, p1.y)))
+                return false;
+
+            if (!CoordIsInPolygon((p1.x, p2.y)))
+                return false;
+
+
+            return true;
+        }
+
+        bool AllEdgesInPolygon((long x, long y) p1, (long x, long y) p2)
+        {
+            //for (long x = Math.Min(p1.x, p2.x); x <= Math.Max(p1.x, p2.x); x++)
+            //{
+            //    for (long y = Math.Min(p1.y, p2.y); y <= Math.Max(p1.y, p2.y); y++)
+            //    {
+            //        if (!CoordIsInPolygon((x,y)))
+            //            return false;
+            //    }
+            //}
+
+            //going right ways
+
+            (long x, long y) c1 = (p2.x, p1.y);
+            (long x, long y) c2 = (p1.x, p2.y);
+
+
+            //p1.x
+            for (long x = Math.Min(p1.x, c1.x); x <= Math.Max(p1.x, c1.x); x++)
+            {
+                if (!CoordIsInPolygon((x, p1.y)))
+                    return false;
+            }
+
+            //p2.x
+            for (long x = Math.Min(p2.x, c2.x); x <= Math.Max(p2.x, c2.x); x++)
+            {
+                if (!CoordIsInPolygon((x, p2.y)))
+                    return false;
+            }
+
+            //p1.y
+            for (long y = Math.Min(p1.y, c2.y); y <= Math.Max(p1.y, c2.y); y++)
+            {
+                if (!CoordIsInPolygon((p1.x, y)))
+                    return false;
+            }
+
+            //p2.y
+            for (long y = Math.Min(p2.y, c1.y); y <= Math.Max(p2.y, c1.y); y++)
+            {
+                if (!CoordIsInPolygon((p2.x, y)))
+                    return false;
+            }
+
+            return true;
+        }
+
+        List<(long size, (long x, long y) c1, (long x, long y) c2)> candidates = new();
+
+        foreach (var c1 in coords)
+        {
+            foreach (var c2 in coords)
+            {
+                if (c2 == c1) continue;
+
+                long size = Size(c1, c2);
+
+                if (AllCornersInPolygon(c1, c2))
+                {
+                    candidates.Add((size, c1, c2));
+                }
+            }
+        }
+
+        foreach(var i in candidates.OrderByDescending(c => c.size))
+        {
+            if (AllEdgesInPolygon(i.c1, i.c2))
+            {
+                result = i.size; 
+                break;
+            }
+        }
+
         Assert.Equal(expected, result);
     }
 }
